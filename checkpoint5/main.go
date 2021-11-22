1a50066677b21c4052e63b4558e722b646cbedd0
jackj-ohn1 <92258893+jackj-ohn1@users.noreply.github.com>
2021-11-22 22:15:57 +0800

    Update main.go

--- checkpoint5/main.go
+++ checkpoint5/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/Grand-Theft-Auto-In-CCNU-MUXI/hacker-support/httptool"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
 )
 
 func main() {
-	req, err := httptool.NewRequest(
+	/*req, err := httptool.NewRequest(
 		"",
 		"",
 		"",
@@ -19,5 +24,29 @@ func main() {
 	fmt.Println(req)
 
 	// write your code below
-	// ...
+	// ...*/
+	url := "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/muxi/backend/computer/examination"
+	buf := new(bytes.Buffer)
+	writer := multipart.NewWriter(buf)
+	contenttype := writer.FormDataContentType()
+	formfile, err := writer.CreateFormFile("file", "permute.go")
+	Error(err)
+	file, err := os.Open("./test.go")
+	Error(err)
+	defer file.Close()
+	_, err = io.Copy(formfile, file)
+	Error(err)
+	writer.Close()
+	req, err := http.NewRequest("POST", url, buf)
+	Error(err)
+	req.Header.Add("code", "yyj")
+	req.Header.Add("passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoieXlqIiwiaWF0IjoxNjM3MjAzODUyLCJuYmYiOjE2MzcyMDM4NTJ9.Jzty8lQi4jaQ8Kzuk22ztYLRKXyi6KYZytc8tRlO1zI")
+	req.Header.Set("Content-Type", contenttype)
+
+	resp, err := http.DefaultClient.Do(req)
+	Error(err)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
+
 }
