aa1adfa9f6a5a29d10c72d198b28809dd9f8e789
jackj-ohn1 <92258893+jackj-ohn1@users.noreply.github.com>
2021-11-22 21:39:20 +0800

    Update main.go

--- checkpoint2/main.go
+++ checkpoint2/main.go
@@ -24,22 +24,15 @@ func main() {
 	client := &http.Client{}
 	url := "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code"
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("code", "yyj")
 
 	req.Header.Add("passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoieXlqIiwiaWF0IjoxNjM3MjA0MDMzLCJuYmYiOjE2MzcyMDQwMzN9.CTGHa5Pq9yAICFsVTC1Xq9dAl3k221j2ERZ2crGlZQ4")
-	if err != nil {
-		fmt.Println(err)
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	resp, _ := client.Do(req)
+
+	content, _ := ioutil.ReadAll(resp.Body)
+	
 	fmt.Println(string(content))
 	defer resp.Body.Close()
 }
